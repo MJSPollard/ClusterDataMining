5ba6b57cb5fdc3cd9439276d9996fa1e5f26f41b
Hughman <hughman25@gmail.com>
2017-12-10 19:26:57 -0700

    Went wrong dir. w/ aco, redoing based on inclass notes

--- Project4_447/src/project4/ACO.java
+++ Project4_447/src/project4/ACO.java
@@ -1,48 +1,43 @@
 package project4;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.lang.Math;
 
 public class ACO
 {
-	private double[][] graph, edges;
+	private double[][] pheromoneMatrix, data;
 	private final int TotalAnts;
 	private Ant[] ants;
-	private double[] probability;
 	private int size, bestLength, maxIterations;
 	private Random random;
 	private int[] bestTour;
+	private final double alpha = 1;
+	private final double beta = 5;
 
-	public ACO(double[][] data)
+	public ACO(double[][] data, int instances, int attr, int clusters)
 	{
 		random = new Random(420);
-		graph = data;
-		size = graph.length;
-		edges = new double[size][size];
+		this.data = data;
+		pheromoneMatrix = new double[instances][clusters];
+
+		size = data.length;
 		TotalAnts = (int) (size * .8);
-		probability = new double[size];
-		
+
+		maxIterations = 500;
+	}
+	
+	public void initialize()
+	{
 		ants = new Ant[TotalAnts];
 		for (int i = 0; i < TotalAnts; i++)
 		{
-			ants[i] = new Ant(size, size);
+			ants[i] = new Ant(data.length, data[0].length, pheromoneMatrix[0].length);
 		}
-		
-		maxIterations = 500;
+
 	}
 
-//	public void initialize()
-//	{
-//		ants = new Ant[TotalAnts];
-//		for (int i = 0; i < TotalAnts; i++)
-//		{
-//			ants[i] = new Ant(size, size);
-//		}
-//
-//		setupAnts();
-//		clearEdges();
-//	}
-	
 	private void setupAnts()
 	{
 		for (int i = 0; i < TotalAnts; i++)
@@ -51,115 +46,74 @@ public class ACO
 			ants[i].visitCity(random.nextInt(size));
 		}
 	}
+
+	private void setupPM()
+	{
+		for (int i = 0; i < pheromoneMatrix.length; i++)
+		{
+			for (int j = 0; j < pheromoneMatrix[0].length; j++)
+			{
+				pheromoneMatrix[i][j] = Math.random() * .05;
+			}
+		}
+	}
+
+	private int moveAnt(Ant ant, int index)
+	{
+		double[] dataPoint = data[index];
+		
+		double[][] clusters = ant.getAttrCenters();
+		
+		double highest = 0;
+		int winnerIndex = 0;
+		
+		for(int i = 0; i < clusters.length; i++)
+		{
+			double divider = 0.0;
+			int cc = 0;
+			
+			for(double[] cluster : clusters)
+			{
+				double pheromoneFactor = Math.pow(pheromoneMatrix[index][cc], alpha);
+				
+				double distanceFactor = calculateDistance(dataPoint, clusters[i]);
+				distanceFactor = Math.pow((1/distanceFactor), beta);
+				
+				divider += pheromoneFactor * distanceFactor;
+				cc++;
+			}
+			
+			double distance = calculateDistance(dataPoint, clusters[i]);
+			distance = Math.pow((1/distance), beta);
+			
+			double pheromone = Math.pow(pheromoneMatrix[index][i], alpha);
+			
+			double total = pheromone * distance;
+			
+            double clusterProb =  total / divider;
+           
+            
+            if(clusterProb > highest)
+            {
+                highest = clusterProb;
+                winnerIndex = i;
+            }
+		}
+		return winnerIndex;
+	}
 	
-	private void clearEdges()
+	private double calculateDistance(double[] dp, double[] cluster)
 	{
-		for (int i = 0; i < size; i++)
+		double dist = 0;
+		
+		for(int i = 0; i < dp.length; i++)
 		{
-			for (int j = 0; j < size; j++)
-			{
-				edges[i][j] = 1.0;
-			}
+			dist += Math.abs(dp[i] - cluster[i]);
 		}
-	}
-
-	private void moveAnts()
-	{
-		// each ant follows edges...
-		int iteration = 0;
-		while (iteration < size - 1)
-		{
-			for (Ant ant : ants)
-				ant.visitCity(calculateNextPos(ant));
-			iteration++;
-		}
-	}
-
-	public int calculateNextPos(Ant ant)
-	{
-		// Small probability to select next position purely randomly
-		if (random.nextDouble() < .05)
-		{
-			int t = random.nextInt(size - ant.trailLength());
-			int j = -1;
-			for (int i = 0; i < size; i++)
-			{
-				if (!ant.visited(i))
-					j++;
-				if (j == t)
-					return i;
-			}
-
-		}
-		// calculate probabilities for each town (stored in probs)
-		probTo(ant);
-		// randomly select according to probs
-		double threshold = random.nextDouble();
-		double chance = 0;
-		for (int i = 0; i < size; i++)
-		{
-			chance += probability[i];
-			if (chance >= threshold)
-				return i;
-		}
-		return -1;
-	}
-
-	// Store in probability array the probability of moving to each town
-	// [1] describes how these are calculated.
-	// In short: As like to follow stronger and shorter edges more.
-	private void probTo(Ant ant)
-	{
-		int currentCity = ant.getCurrentCity();
-		System.out.println(currentCity);
-
-		double lower = 0.0;
-
-		for (int i = 0; i < size; i++)
-		{
-			System.out.print(i);
-			if (!ant.visited(i))
-			{
-				double one = Math.pow(edges[currentCity][i], 1);
-				double two = Math.pow(1 / graph[currentCity][i], 5);
-				lower += one * two;
-			}
-
-		}
-		for (int i = 0; i < size; i++)
-		{
-			if (ant.visited(i))
-			{
-				probability[i] = 0.0;
-			} else
-			{
-				double upper = Math.pow(edges[currentCity][i], 1) * Math.pow(1.0 / graph[currentCity][i], 5);
-
-				probability[i] = upper / lower;
-			}
-		}
-
-	}
-
-	// Update edges based on ants tours
-	private void updateTrails()
-	{
-		// evaporation
-		for (int i = 0; i < size; i++)
-			for (int j = 0; j < size; j++)
-				edges[i][j] *= .5;
-
-		// each ants contribution
-		for (Ant ant : ants)
-		{
-			int[] trail = ant.getTrail();
-			double contribution = 500 / ant.trailLength();
-			for (int i = 0; i < size - 1; i++)
-			{
-				edges[trail[i]][trail[i + 1]] += contribution;
-			}
-			edges[trail[size - 1]][trail[0]] += contribution;
-		}
+		
+		dist = dist / dp.length;
+		
+		return dist;
 	}
 
 	private void updateBest()
@@ -179,9 +133,9 @@ public class ACO
 		}
 	}
 
-	public int[] solve()
+	public int[] run()
 	{
-		clearEdges();
+		setupPM();
 
 		int iter = 0;
 		// run for maxIterations
@@ -189,14 +143,41 @@ public class ACO
 		while (iter < maxIterations)
 		{
 			setupAnts();
-			moveAnts();
-			updateTrails();
-			updateBest();
+
+			for (Ant ant : ants)
+			{
+                ArrayList<Integer> randomDataCounter = getRandomCounter();
+                
+    			int k = 0;
+                while(ant.finish())
+                {
+                	int dataIndex = randomDataCounter.get(k);
+                	
+            		ant.addWinner(dataIndex, moveAnt(ant, dataIndex));
+            		
+    				updateTrails();
+    				k++;
+                }
+                
+				updateBest();
+			}
 			iter++;
 		}
-		// Subtract size because we added one to edges on load
+		// Subtract size because we added one to pheromones on load
 		System.out.println("Best tour length: " + (bestLength - size));
 		return bestTour.clone();
+	}
+
+	public ArrayList<Integer> getRandomCounter()
+	{
+		ArrayList<Integer> dataIndex = new ArrayList<>();
+		for(int i = 0; i < size; i++)
+		{
+		    dataIndex.add(i);
+		}
+		Collections.shuffle(dataIndex);
+		
+		return dataIndex;
 	}
 
 }
--- Project4_447/src/project4/Ant.java
+++ Project4_447/src/project4/Ant.java
@@ -5,14 +5,15 @@ import java.util.ArrayList;
 public class Ant
 {
 	private ArrayList<Integer> trail;
+	private double[][] attrCenters;
 	private boolean[] visited;
 	
-	public Ant(int instances, int attributes)
+	public Ant(int instances, int attr, int clusters)
 	{
-		visited = new boolean[instances * attributes];
-       // public int trail[] = new int[graph.length];
-		trail = new ArrayList<Integer>();
 		visited = new boolean[instances];
+		
+		attrCenters = new double[clusters][attr];
+		trail = new ArrayList<Integer>();
 	}
 	
 	public void visitCity(int city)
@@ -40,6 +41,70 @@ public class Ant
     	int[] arrayTrail = convertToArray(trail);
     	return arrayTrail;
     }
+    public void addWinner(int index, int winnerIndex)
+    {
+    	visited[index] = true;
+    	int loop = attrCenters[index].length;
+    	
+    	for(int i = 0; i < loop; i++)
+    	{
+    		if(i == winnerIndex)
+    		{
+    			attrCenters[index][i] = 1;
+    		}
+    		else
+    		{
+    			attrCenters[index][i] = 0;
+    		}
+    	}
+    }
+    
+    public void updateCenter(double[][] data)
+    {
+    	for(int cluster = 0; cluster < attrCenters.length; cluster++)
+    	{
+    		int count = 0;
+    		for(int instance = 0; instance < visited.length; instance++)
+    		{
+    			if(visited[instance])
+    			{
+    				for(int attr = 0; attr < attrCenters[cluster].length; attr++)
+					{
+    					if(count == 0)
+    					{
+    						attrCenters[cluster][attr] = 0;
+    					}
+    					attrCenters[cluster][attr] += data[instance][attr];
+					}
+    				count++;
+    			}
+    		}
+    		for(int attr = 0; attr < attrCenters[cluster].length; attr++)
+    		{
+    			if(count > 0)
+    			{
+    				attrCenters[cluster][attr] = attrCenters[cluster][attr] / count;
+    			}
+    		}
+    	}
+    }
+    
+    public double[][] getAttrCenters()
+    {
+    	return attrCenters;
+    }
+    
+    public boolean finish()
+    {
+    	for(boolean visit: visited)
+    	{
+    		if(!visit)
+    		{
+    			return false;
+    		}
+    	}
+    	return true;
+    }
 
     public void clear()
     {
@@ -63,10 +128,5 @@ public class Ant
             result[i] = intList.get(i);
         }
         return result;
-    }
-    
-    public String toString()
-    {
-    	return "h";
     }
 }
